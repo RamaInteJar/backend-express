565fadf0c70a03571ff78f5f63b5c1ec0c48ef14
pentiumKing <ramahawai87@gmail.com>
2023-09-25 18:09:31 -0400

    defined cors and add its middleware

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const app = express()
 const bookmarkController = require('./controllers/bookmarkController')
 const PORT = process.env.PORT || 7050
 const morgan = require('morgan')
+const cors = require("cors")
 
 //Middleware 
+app.use(cors()) //this allow the frontend to acess data from the backend
 app.use(morgan('tiny'))
 app.use(express.urlencoded({extended: true}))
 
